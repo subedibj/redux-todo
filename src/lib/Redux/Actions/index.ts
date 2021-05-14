b42a644e43db17f7f54005a38d66f1ea237219bd
bijay <bijayqubits@gmail.com>
2021-05-14 18:36:58 +0545

    todo list edit functionality is added

--- src/lib/Redux/Actions/index.ts
+++ src/lib/Redux/Actions/index.ts
@@ -11,18 +11,23 @@ export const addTodo = (data) => {
 export const deleteTodo = (id) => {
   return {
     type: "DELETE_TODO",
-    id,
+    payload: {
+      id,
+    },
   };
 };
 
 export const removeTodo = () => {
-    return{
-        type: "REMOVE_TODO",
-    }
-}
-export const editTodo = (id) => {
-    return{
-        type: "EDIT_TODO",
-        id,
-    }
-}
+  return {
+    type: "REMOVE_TODO",
+  };
+};
+export const editTodo = ({ id, data }) => {
+  return {
+    type: "EDIT_TODO",
+    payload: {
+      id,
+      data,
+    },
+  };
+};
--- src/lib/Redux/Reducers/todoReducer.ts
+++ src/lib/Redux/Reducers/todoReducer.ts
@@ -1,4 +1,3 @@
-
 const initalData = {
   list: [],
 };
@@ -6,39 +5,46 @@ const todoReducers = (state = initalData, action) => {
   switch (action.type) {
     case "ADD_TODO":
       const { id, data } = action.payload;
-      if(data && data.length > 3){
+      if (data && data.length > 3) {
         return {
-            ...state,
-            list: [
-              ...state.list,
-              {
-                id: id,
-                data: data,
-              },
-            ],
-          };
+          ...state,
+          list: [
+            ...state.list,
+            {
+              id: id,
+              data: data,
+            },
+          ],
+        };
+      } else {
+        alert("Data must be in 3 character");
       }
-      else{
-          alert("Data must be in 3 character")
-      }
-      
+
     case "DELETE_TODO":
-      const deleteItem = state.list.filter((elem) => elem.id !== action.id);
+      const deleteItem = state.list.filter(
+        (elem) => elem.id !== action.payload.id
+      );
       return {
         ...state,
         list: deleteItem,
       };
     case "REMOVE_TODO":
       return {
-          ...state,
-          list:[]
+        ...state,
+        list: [],
       };
-      case "EDIT_TODO" : 
-      const editItem = state.list.filter((elem)=> elem.id == action.id)
-      return{
-          ...state,
-        
-      }
+    case "EDIT_TODO":
+      const editedItem = state.list.map((elem) => {
+        if (elem.id === action.payload.id) {
+          return action.payload;
+        } else {
+          return elem;
+        }
+      });
+      return {
+        ...state,
+        list: editedItem,
+      };
     default:
       return state;
   }
